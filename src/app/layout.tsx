f41757dd110c705baf193d23f78bb57772f2a8b0
vascodaga <y.m19900603@icloud.com>
2025-10-01 18:18:09 +0900

    en⇒jp

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -35,7 +35,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="jp">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         {children}
       </body>
