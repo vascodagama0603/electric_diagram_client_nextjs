53100e5e7eb1a55dbc423da77c9ed46ad64acb40
vascodaga <y.m19900603@icloud.com>
2025-10-25 12:16:59 +0900

    Suspense

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -2,7 +2,7 @@ import { PageLayout } from '../components/LayoutComponents';
 import { StyledContentContainer } from '../components/ContentStyles';
 // 💡 修正: ClientTagFilterWithData ラッパーをインポート
 import ClientTagFilterWithData from '../components/ClientTagFilterWithData'; 
-
+import React, { Suspense } from 'react';
 // 💡 修正: searchParams 引数を削除し、純粋なサーバーコンポーネントにする
 export default async function BlogPage() {
     
@@ -10,7 +10,9 @@ export default async function BlogPage() {
         <PageLayout>
             <StyledContentContainer>
                 {/* 💡 データ取得とフィルターロジックを ClientTagFilterWithData に委譲 */}
-                <ClientTagFilterWithData />
+                <Suspense fallback={<div>ブログ一覧をロード中...</div>}>
+                    <ClientTagFilterWithData />
+                </Suspense>
             </StyledContentContainer>
         </PageLayout>
     );
